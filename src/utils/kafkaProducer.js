4c75d43fae60e77b9da7376e659fba6403d9e682
joffinjoy <joffinjoy@gmail.com>
2023-01-02 19:18:43 +0530

    Modified Kafka Producer

--- src/utils/kafkaProducer.js
+++ src/utils/kafkaProducer.js
@@ -1,12 +1,18 @@
 'use strict'
 const { Kafka } = require('kafkajs')
-const kafka = new Kafka({ clientId: process.env.KAFKA_CLIENT_ID, brokers: process.env.KAFKA_BROKERS.split(' ') })
+const kafka = new Kafka({ clientId: process.env.KAFKA_CLIENT_ID, brokers: process.env.KAFKA_BROKERS.split(',') })
 const producer = kafka.producer()
+
+producer.on('producer.connect', () => console.log('Kafka Producer Connected'))
+producer.on('producer.disconnect', () => console.log('Kafka Producer Disconnected'))
+//producer.on('producer.network.request', (request) => console.log('Producer Request: ', request))
 
 const produce = (topic) => async (data) => {
 	try {
-		data.topic = topic
-		await producer.connect()
+		console.log('PRODUCER TOPIC: ', topic)
+		console.log('PRODUCER DATA: ', data)
+		//data.topic = topic
+		//await producer.connect() //Add this back if producer ever gets garbage collected. Unlikely
 		await producer.send({
 			topic,
 			messages: [
@@ -22,5 +28,8 @@ const produce = (topic) => async (data) => {
 }
 
 exports.kafkaProducers = {
-	session: produce(process.env.KAFKA_SESSION_TOPIC),
+	session: produce(process.env.KAFKA_SESSION_TOPIC_ELASTIC),
 }
+
+exports.initialize = () =>
+	producer.connect().catch((error) => console.log('Kafka Producer Initialization Error: ', error))
