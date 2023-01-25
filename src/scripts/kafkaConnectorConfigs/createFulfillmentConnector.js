c285afc6ed5c24f2ff52f396eff9a84266af6156
joffinjoy <joffinjoy@gmail.com>
2023-01-25 11:51:30 +0530

    Added Script Files For Kakfa-Connect Connector Configuration

---
+++ src/scripts/kafkaConnectorConfigs/createFulfillmentConnector.js
@@ -0,0 +1,34 @@
+'use strict'
+const axios = require('axios')
+require('dotenv').config({ path: '../../.env' })
+
+const data = JSON.stringify({
+	'connector.class': 'io.confluent.connect.elasticsearch.ElasticsearchSinkConnector',
+	'connection.url': process.env.ELASTIC_NODE,
+	'key.ignore': false,
+	'schema.ignore': 'true',
+	'write.method': 'UPSERT',
+	topics: process.env.KAFKA_FULFILLMENT_ELASTIC_TOPIC,
+	'type.name': 'doc',
+	'value.converter.schemas.enable': 'false',
+	'key.converter': 'org.apache.kafka.connect.storage.StringConverter',
+	'value.converter': 'org.apache.kafka.connect.json.JsonConverter',
+	index: process.env.ELASTIC_FULFILLMENT_INDEX,
+})
+
+const config = {
+	method: 'put',
+	url: `${process.env.KAFKA_CONNECT_URI}/connectors/${process.env.KAFKA_CONNECT_FULFILLMENT_CONNECTOR}/config`,
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	data: data,
+}
+
+axios(config)
+	.then(function (response) {
+		console.log(JSON.stringify(response.data, null, 2))
+	})
+	.catch(function (error) {
+		console.log(error)
+	})
